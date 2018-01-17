78b416c46740184a84f65af391995610a84abdd3
dennisja <dennisjjagwe@gmail.com>
2018-01-17 23:42:17 +0300

    [Ft #154304915] Implement change password Edit change password component to beable to make requests to the api inorder to change the user password

--- src/components/forms/ChangePasswordForm.js
+++ src/components/forms/ChangePasswordForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import User from '../../helpers/User';
 
 export default class ChangePasswordForm extends Component {
     state = {
@@ -7,7 +8,7 @@ export default class ChangePasswordForm extends Component {
         new_password_again:"",
     }
 
-    handleInpuChange= (event)=>{
+    handleInputChange = (event)=>{
         const target = event.target;
         this.setState({
             [target.name]:target.value
@@ -16,8 +17,15 @@ export default class ChangePasswordForm extends Component {
 
     handleFormSubmit = (event) =>{
         event.preventDefault();
+        alert('Yeah')
         //make api request to change password here
-        alert(this.state);
+        User.changeUserPassword(this.state)
+        .then(response=>{
+            console.log(response);
+        })
+        .catch(error=>{
+            console.log(error)
+        })
     }
 
     render() {
@@ -25,15 +33,15 @@ export default class ChangePasswordForm extends Component {
           <form onSubmit={this.handleFormSubmit}>
             <div className="input-field">
               <label htmlFor="current_password">Current Password</label>
-              <input type="password" name="current_password" id="current_password" onChange={this.handleInpuChange} required />
+              <input type="password" name="current_password" id="current_password" onChange={this.handleInputChange} required />
             </div>
             <div className="input-field">
               <label htmlFor="new_password">New Password </label>
-              <input type="password" name="new_password" id="new_password" onChange={this.handleInpuChange} required />
+              <input type="password" name="new_password" id="new_password" onChange={this.handleInputChange} required />
             </div>
             <div className="input-field">
               <label htmlFor="new_password_again">Confirm New Password: </label>
-              <input type="password" name="new_password_again" id="new_password_again" onChange={this.handleInpuChange} required />
+              <input type="password" name="new_password_again" id="new_password_again" onChange={this.handleInputChange} required />
             </div>
             <div className="center-align">
                 <input type="submit" className="btn btn-small orange" value="Change Password" name="change_password" id="change_pasword" />
